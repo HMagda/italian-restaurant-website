ace4c28367b0f57cc6b82a202d6f2888d2df0700
Magdalena <magdalenahyra@wp.pl>
2022-09-22 17:06:20 +0200

    amount widget

--- src/js/script.js
+++ src/js/script.js
@@ -63,6 +63,7 @@
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
+      thisProduct.initAmountWidget();
       thisProduct.processOrder();
     }
     renderInMenu() {
@@ -144,15 +145,60 @@
           }
         }
       }
+      price *= thisProduct.amountWidget.value;
       thisProduct.priceElem.innerHTML = price;
+    }
+    initAmountWidget() {
+      const thisProduct = this;
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+      thisProduct.amountWidgetElem.addEventListener('updated', thisProduct.processOrder();)
     }
   }
 
   class AmountWidget {
     constructor(element){
       const thisWidget = this;
+      thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value);
+      thisWidget.initActions();
       console.log('AmountWidget:', AmountWidget);
       console.log('constructor arguments:', element);
+    }
+    getElements(element){
+      const thisWidget = this;
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+    setValue(value) {
+      const thisWidget = this;
+      const newValue = parseInt(value);
+
+      //to do: add validation
+      if (thisWidget.value !== newValue && !isNaN(newValue) && settings.amountWidget.defaultMin <= newValue <= settings.amountWidget.defaultMax) {
+        thisWidget.value = newValue;
+      }
+     
+      settings.amountWidget.defaultValue = thisWidget.value;
+
+      announce() {
+        const thisWidget = this;
+        const event = new WheelEvent('updated');
+        thisWidget.element.dispatchEvent(event);
+      }
+
+    }
+    initActions() {
+      thisWidget.input.addEventListener('change', (event) =>{
+        setValue(value);
+      });
+      thisWidget.linkDecrease.addEventListener('click', (event) => {
+        setValue(thisWidget.value - 1)
+      });
+      thisWidget.linkIncrease.addEventListener('click', (event) => {
+        setValue(thisWidget.value + 1)
+      });
     }
   }
 
