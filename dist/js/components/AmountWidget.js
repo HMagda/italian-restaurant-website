594e61abd82a9698eb1b3421b8de40cbd0cf60ba
Magdalena <magdalenahyra@wp.pl>
2022-10-06 20:16:48 +0200

    Added date and time picker in the booking section

--- dist/js/components/AmountWidget.js
+++ dist/js/components/AmountWidget.js
@@ -1,58 +1,53 @@
 import {settings, select} from '../settings.js';
+import BaseWidget from './BaseWidget.js';
 
-
-class AmountWidget {
+class AmountWidget extends BaseWidget{
 
   constructor(element){
+    super(element, settings.amountWidget.defaultValue);
+
     const thisWidget = this;
+
     thisWidget.getElements(element);
-    thisWidget.setValue(thisWidget.input.value || settings.amountWidget.defaultValue);
+    // thisWidget.setValue(thisWidget.input.value || settings.amountWidget.defaultValue);
     thisWidget.initActions();
   }
 
-  getElements(element) {
+  getElements() {
     const thisWidget = this;
 
-    thisWidget.element = element;
-    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
   }
 
-  setValue(value) {
-    const thisWidget = this;
-    const newValue = parseInt(value);
-
-    if ((thisWidget.value !== newValue) && !isNaN(newValue) && (settings.amountWidget.defaultMin <= newValue) && (newValue <= settings.amountWidget.defaultMax)) {
-      thisWidget.value = newValue;
-      this.announce();
-    }
-     
-    thisWidget.input.value = thisWidget.value;
+  isValid(value){
+    return !isNaN(value) 
+    && value >= settings.amountWidget.defaultMin 
+    && value <= settings.amountWidget.defaultMax;
   }
 
-  announce() {
+  renderValue(){
     const thisWidget = this;
 
-    const event = new CustomEvent('updated', {
-      bubbles: true
-    });
-
-    thisWidget.element.dispatchEvent(event);
+    thisWidget.dom.input.value = thisWidget.value;
   }
 
   initActions() {
     const thisWidget = this;
-    thisWidget.input.addEventListener('change', () => this.setValue(this.input.value));
+    thisWidget.dom.input.addEventListener('change', function(){
+      // thisWidget.setValue(thisWidget.dom.input.value);
+      thisWidget.value = thisWidget.dom.input.value;
+    });
      
-    thisWidget.linkDecrease.addEventListener('click', (event) => {
+    thisWidget.dom.linkDecrease.addEventListener('click', function(event){
       event.preventDefault();
-      this.setValue(this.value - 1);
+      thisWidget.setValue(thisWidget.value - 1);
     });
 
-    thisWidget.linkIncrease.addEventListener('click', (event) => {
+    thisWidget.dom.linkIncrease.addEventListener('click', function(event){
       event.preventDefault();
-      this.setValue(this.value + 1);
+      thisWidget.setValue(thisWidget.value + 1);
     });
   }
 }
--- dist/js/components/Booking.js
+++ dist/js/components/Booking.js
@@ -1,6 +1,7 @@
 import {templates, select} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
-
+import HourPicker from './HourPicker.js';
+import DatePicker from './DatePicker.js';
 
 class Booking {
   constructor(element) {
@@ -18,6 +19,8 @@ class Booking {
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
 
   }
 
@@ -25,8 +28,10 @@ class Booking {
     const thisBooking = this;
 
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
-
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
   }
 }
 
